e053c66b0535245e6af31b5378ffce36d31c1b5f
smallfatcat <david.imrie@gmail.com>
2017-11-11 06:29:52 +0000

    continued work on map.js

--- dev/getMapData.php
+++ dev/getMapData.php
@@ -11,13 +11,20 @@
 		  die('Connection failed: ' . $e->getMessage());
 		}
 
-		$ksql = $conn->prepare("SELECT solar_system_id, solar_system_name, constellation_id, constellation_name, region_id, region_name, x, y, z FROM nerdDB.map WHERE map.region_id = ?");
+		$ksql = $conn->prepare("SELECT solar_system_id, solar_system_name, constellation_id, constellation_name, region_id, region_name, x, y, z, render_x, render_y FROM nerdDB.map WHERE map.region_id = ?");
+		$lsql = $conn->prepare("SELECT toSolarSystemID FROM eve_sde.mapSolarSystemJumps WHERE fromSolarSystemID = ?");
 		$res = $ksql -> execute(array($region_id));
 
-		$resultArray[0] = Array('solar_system_id','solar_system_name','constellation_id','constellation_name','region_id','region_name','x','y','z');
-		for($i=1;$i<=$ksql->rowCount();$i++){
+		//$resultArray[0] = Array('solar_system_id','solar_system_name','constellation_id','constellation_name','region_id','region_name','x','y','z','render_x','render_y','linkList');
+		for($i=0;$i<$ksql->rowCount();$i++){
 			$row = $ksql->fetch(PDO::FETCH_ASSOC);
-			$resultArray[$i] = Array($row['solar_system_id'], $row['solar_system_name'], $row['constellation_id'], $row['constellation_name'], $row['region_id'], $row['region_name'], $row['x'],$row['y'], $row['z']);
+			$resLink = $lsql -> execute(array($row['solar_system_id']));
+			$linkArray = Array();
+			for($j=0;$j<$lsql->rowCount();$j++){
+				$linkRow = $lsql->fetch(PDO::FETCH_ASSOC);
+				$linkArray[$j] = Array($linkRow['toSolarSystemID']); 
+			}
+			$resultArray[$i] = Array($row['solar_system_id'], $row['solar_system_name'], $row['constellation_id'], $row['constellation_name'], $row['region_id'], $row['region_name'], $row['x'],$row['y'], $row['z'],$row['render_x'], $row['render_y'], $linkArray);
 		}
 		echo json_encode($resultArray);
 	}
--- dev/map.js
+++ dev/map.js
@@ -7,50 +7,135 @@
 
 var region_id = 10000001;
 
-var g_map_data = new Array();
+var g_map_data = {};
+var g_raw_map_data = [];
+var g_jump_data_array = [];
 var g_highlight_solar_system = 0;
+var g_highlight_index = 0;
+var canvas;
+var ctx;
+
 
 // Document Ready
 $( function() {
 	get_map_data(region_id);
+	//$('#map_canvas').onclick(update_map_data);
+	canvas = document.getElementById("map_canvas");
+	ctx = canvas.getContext("2d");
+
+	ctx.canvas.addEventListener('click', function(event){
+  	update_map_data(event.clientX-ctx.canvas.offsetLeft, event.clientY-ctx.canvas.offsetTop);
+  });
 } );
 
-function save_map_data(map_data){
-	g_map_data = map_data;
-	draw_table_result(g_map_data);
+function update_map_data(x,y){
+	g_map_data[g_highlight_solar_system].x_view = x;
+	g_map_data[g_highlight_solar_system].y_view = y;
+	//console.log(g_map_data[highlight_index]);
 	draw_map(g_map_data);
+	draw_table_result(g_map_data);
 }
 
-function draw_map(map_data){
+function save_map_data(raw_map_data){
+	process_raw_map_data(raw_map_data);
+	scale_xy();
+	draw_map(g_map_data);
+	draw_table_result(g_map_data);
+}
+
+function process_raw_map_data(raw_map_data){
 	
+	g_raw_map_data = raw_map_data;
 
-	var canvas = document.getElementById("map_canvas");
-  var ctx = canvas.getContext("2d");
+	var map_data = {};
+	raw_map_data.forEach(function(rm){
+		map_data[ rm[0] ] = {
+			'x': rm[6],
+			'y': rm[7],
+			'z': rm[8],
+			'name': rm[1],
+			'id': rm[0],
+			'x_view': rm[9],
+			'y_view': rm[10]
+		};
+	});
+	g_map_data = map_data;
+	
+	// Create jump array
+	var jump_data_array = [];
+	raw_map_data.forEach(function(ss){
+		var from = ss[0];
+		ss[11].forEach(function(to){
+			var jump = {
+				'from': from,
+				'to':  to[0]
+			};
+			jump_data_array.push(jump);
+		});
+	});
+	g_jump_data_array = jump_data_array;
+	
+	
+}
 
+function make_ss_view_array(map_data){
+	var ss_view_array = [];
+	for(var ss in map_data){
+		var ss_view = {
+			'x': map_data[ss].x_view,
+			'y': map_data[ss].y_view,
+			'name': map_data[ss].name,
+			'id': map_data[ss].id
+		};
+		ss_view_array.push(ss_view);
+	}
+	return ss_view_array;
+}
+
+/*function set_highlight_index(){
+	var highlight_index = 0;
+	for(var i=1;i<g_map_data.length;i++){
+		if(g_highlight_solar_system == g_map_data[i][0]){
+			highlight_index = i;
+			break;
+		}
+	}
+	g_highlight_index = highlight_index;
+}*/
+
+function draw_map(map_data){
+  var ss_view_array = make_ss_view_array(g_map_data);
   ctx.font="12px Cuprum";
   
   // Clear
   ctx.fillStyle='rgb(255,255,255)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   //var headings = map_data.shift();
-  map_data.forEach(function(solar_systems){
-  	var scale_factor = 1/10e16*500; 
-  	var x = solar_systems[6]*scale_factor+600;
-  	var y = solar_systems[7]*scale_factor+600;
-  	var z = solar_systems[8]*scale_factor+600;
-  	var name = solar_systems[1]
-  	console.log('solar_system_name: ' + name + ' x: ' + x + ' y: ' + y + ' z: ' + z);
-  	if(g_highlight_solar_system == solar_systems[0]){
+  ss_view_array.forEach(function(solar_systems){
+  	var x = solar_systems.x;
+  	var y = solar_systems.y;
+  	var name = solar_systems.name;
+  	//console.log('solar_system_name: ' + name + ' x: ' + x + ' y: ' + y + ' z: ' + z);
+  	if(g_highlight_solar_system == solar_systems.id){
   		ctx.fillStyle = 'rgb(255, 64, 64)';
   	}else{
   		ctx.fillStyle = 'rgb(64, 255, 64)';
   	}
-  	ctx.fillRect(x, z, 5, 5);
+  	ctx.fillRect(x, y, 5, 5);
   	ctx.fillStyle='rgb(0, 0, 0)';
-  	ctx.fillText(name, x - ctx.measureText(name).width/2, z);
+  	ctx.fillText(name, x - ctx.measureText(name).width/2, y);
   });
- 
+  //map_data.unshift(headings);
+}
 
+function scale_xy(){
+	var scale_factor = 1/10e16*500;
+	for(ss in g_map_data){
+		var x = Math.round(g_map_data[ss].x*scale_factor+600);
+		var z = Math.round(g_map_data[ss].z*scale_factor+600);
+		g_map_data[ss].x_view = x;
+		g_map_data[ss].y_view = z;
+	}
 }
 
 function get_map_data(region_id){
@@ -71,188 +156,12 @@ if (window.XMLHttpRequest) {
 	xmlhttp.send();
 }
 
-function make_autocomplete_list(bigvar){
-	var list = [];
-	bigvar.forEach(function(bigVarObject){
-		list.push(bigVarObject.N);
-	});
-	return list;
-}
-
-function nameToID(name, list){
-	var id = 0;
-	for(var i=0;i<list.length;i++){
-		if(list[i].N.toLowerCase()==name.toLowerCase()){
-			id = list[i].I;
-			break;
-		}
-	}
-	return id;
-}
-
-function attach_autocomplete(id, list){
-	$( id ).autocomplete({
-    source: function( request, response ) {
-        var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
-        response( $.grep( list, function( item ){
-          if(request.term.length>1){
-            return matcher.test( item );
-          }
-        }) );
-    }
-  });
-}
-
-function input_solar_system_click(){
-	var solar_system_name = $('#input_solar_system').val();
-	var solar_system_id = nameToID(solar_system_name, solarSystems)
-	get_data_solar_system(solar_system_id);
-	console.log(solar_system_name);
-	console.log(solar_system_id);
-}
-
-function input_ship_type_click(){
-	var ship_name = $('#input_ship_type').val();
-	var ship_id = nameToID(ship_name, ship_types);
-	get_data_ship_type(ship_id);
-	console.log(ship_name);
-	console.log(ship_id);
-}
-
-function input_character_name_click(){
-	var character_name = $('#input_character_name').val();
-	//var character_id = nameToID(ship_name, ship_types);
-	get_data_character_name(character_name);
-	console.log(character_name);
-	//console.log(character_id);
-}
-
-function handle_character_name_keypress(event){
-	var key=event.keyCode || event.which;
-	if (key==13){
-    input_character_name_click();
-	}
-}
-
-function handle_ship_type_keypress(event){
-	var key=event.keyCode || event.which;
-	if (key==13){
-    input_ship_type_click();
-	}
-}
-
-function handle_solar_system_keypress(event){
-	var key=event.keyCode || event.which;
-	if (key==13){
-    input_solar_system_click();
-	}
-}
-
-function get_kill_details(killmail_id){
-	get_data_kill(killmail_id);
-	get_items(killmail_id);
-}
 
 function highlight_solar_system(solar_system_id){
 	console.log(solar_system_id);
 	g_highlight_solar_system = solar_system_id;
+	//set_highlight_index();
 	draw_map(g_map_data);
-}
-
-function get_data_character_name(character_name){
-if (window.XMLHttpRequest) {
-  // code for modern browsers
-  xmlhttp = new XMLHttpRequest();
-	} else {
-  // code for old IE browsers
-  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-	    var search_data = JSON.parse(this.responseText);
-	    draw_table_result(search_data)
-	    return search_data;
-		}
-	};
-	xmlhttp.open("GET", "https://www.eve-nerd.com/dev/searchKills_character_name.php?character_name="+character_name, true);
-	xmlhttp.send();
-}
-
-function get_data_solar_system(solar_system_id){
-if (window.XMLHttpRequest) {
-  // code for modern browsers
-  xmlhttp = new XMLHttpRequest();
-	} else {
-  // code for old IE browsers
-  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-	    var search_data = JSON.parse(this.responseText);
-	    draw_table_result(search_data)
-	    return search_data;
-		}
-	};
-	xmlhttp.open("GET", "https://www.eve-nerd.com/dev/searchKills_solar_system.php?solar_system_id="+solar_system_id, true);
-	xmlhttp.send();
-}
-
-function get_data_ship_type(ship_type_id){
-if (window.XMLHttpRequest) {
-  // code for modern browsers
-  xmlhttp = new XMLHttpRequest();
-	} else {
-  // code for old IE browsers
-  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-	    var search_data = JSON.parse(this.responseText);
-	    draw_table_result(search_data)
-	    return search_data;
-		}
-	};
-	xmlhttp.open("GET", "https://www.eve-nerd.com/dev/searchKills_ship_type.php?ship_type_id="+ship_type_id, true);
-	xmlhttp.send();
-}
-
-function get_data_kill(killmail_id){
-	if (window.XMLHttpRequest) {
-	  // code for modern browsers
-	  xmlhttp = new XMLHttpRequest();
-		} else {
-	  // code for old IE browsers
-	  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-	    var search_data = JSON.parse(this.responseText);
-	    draw_table_kill_detail(search_data)
-	    return search_data;
-		}
-	};
-	xmlhttp.open("GET", "https://www.eve-nerd.com/dev/getKillDetails.php?killmail_id="+killmail_id, true);
-	xmlhttp.send();
-}
-
-function get_items(killmail_id){
-
-	if (window.XMLHttpRequest) {
-	  // code for modern browsers
-	  xmlhttp = new XMLHttpRequest();
-		} else {
-	  // code for old IE browsers
-	  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-	    var search_data = JSON.parse(this.responseText);
-	    draw_table_kill_items(search_data)
-	    return search_data;
-		}
-	};
-	xmlhttp.open("GET", "https://www.eve-nerd.com/dev/getItems.php?killmail_id="+killmail_id, true);
-	xmlhttp.send();
 }
 
 function draw_table_result(result_array){
@@ -260,37 +169,30 @@ function draw_table_result(result_array){
 	$('#table_result').append(generate_html_table(result_array));
 }
 
-function draw_table_kill_detail(result_array){
-	$('#table_kill_detail').empty();
-	$('#table_kill_detail').append(generate_html_table(result_array));
-}
-
-function draw_table_kill_items(result_array){
-	$('#table_kill_items').empty();
-	$('#table_kill_items').append(generate_html_table(result_array));
-}
-
 function generate_html_table(result_array){
 	var table_string = '';
 	table_string += '<table>';
 	table_string += '<tr>';
-	var header_row = result_array.shift();
-	header_row.forEach(function(element){
-		table_string += '<th>';
-		table_string += element;
-		table_string += '</th>';
-	});
+	var header_row = ['solar_system_id','solar_system_name','constellation_id','constellation_name','region_id','region_name','x','y','z','render_x','render_y'];
+	for(i in result_array){
+		for(element in result_array[i]){
+			table_string += '<th>';
+			table_string += element;
+			table_string += '</th>';
+		}
+		break;
+	}
 	table_string += '</tr>';
-	result_array.forEach(function(data_rows){
-  	table_string += '<tr>';
-  	var solar_system_id = data_rows[0];
-  	data_rows.forEach(function(element){
-	  	table_string += '<td>';
-	  	table_string += '<a onclick="highlight_solar_system('+ solar_system_id +')">' + element + '</a>';
+	for(data_rows in result_array){
+		table_string += '<tr>';
+  	var solar_system_id = result_array[data_rows].id;
+  	for(element in result_array[data_rows]){
+  		table_string += '<td>';
+	  	table_string += '<a onclick="highlight_solar_system('+ solar_system_id +')">' + result_array[data_rows][element] + '</a>';
 	  	table_string += '</td>';
-  	});
+  	}
   	table_string += '</tr>';
-	});
+	}
 	table_string += '</table>';
 	return table_string;
 }
